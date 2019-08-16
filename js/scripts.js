a1f9a3049786046cd2bba20b047425a997da2dac
Nikhil Batta <nbatta08@gmail.com>
2019-08-16 13:29:24 -0700

    adds the logic for the if total score is less than 5

--- js/scripts.js
+++ js/scripts.js
@@ -3,52 +3,54 @@ function refresh(){
   $(".field").val(0);
   $(".field").removeClass("one")
 }
-function checkErrors(){
-  var fields = $(".field").get();
-  var hasErrors = 0;
-  fields.forEach(function(questionIndex){
-    var fieldValue = parseInt($(questionIndex).val());
-
-    if (fieldValue === 0){
-      $(questionIndex).addClass("one");
-      hasErrors = true;
-    }
-    else {
-      $(questionIndex).removeClass("one")
-        console.log(questionIndex);
-    }
-  });
-  return hasErrors;
-}
-
-function calculateScore(){
-  var fields = $(".field").get();
-  var totalScore = 0 ;
-  fields.forEach(function(questionIndex){
-    var fieldValue = parseInt($(questionIndex).val());
-    totalScore = totalScore + fieldValue;
-    });
-  return totalScore;
-}
-
+// function checkErrors(){
+//   var fields = $(".field").get();
+//   var hasErrors = 0;
+//   fields.forEach(function(questionIndex){
+//     var fieldValue = parseInt($(questionIndex).val());
+//
+//     if (fieldValue === 0){
+//       $(questionIndex).addClass("one");
+//       hasErrors = true;
+//     }
+//     else {
+//       $(questionIndex).removeClass("one")
+//         console.log(questionIndex);
+//     }
+//   });
+//   return hasErrors;
+// }
+//
+// function calculateScore(){
+//   var fields = $(".field").get();
+//   var totalScore = 0 ;
+//   fields.forEach(function(questionIndex){
+//     var fieldValue = parseInt($(questionIndex).val());
+//     totalScore = totalScore + fieldValue;
+//     });
+//   return totalScore;
+// }
+// function errorChanger(){
+//
+// }
 
 $(document).ready(function(){
   $("#formOne").submit(function(event){
     event.preventDefault();
-    // var trait = parseInt($("#trait").val());
-    // var whyCode = parseInt($("#whyCode").val());
-    // var skill = parseInt($("#skill").val());
-    // var interest = parseInt($("#interest").val());
-    // var laptopPreference = parseInt($("#laptop").val());
-    // console.log(trait)
-    // console.log(whyCode)
-    // console.log(skill)
-    // console.log(interest)
-    // console.log(laptopPreference)
-    //var totalScore = trait + whyCode + skill + interest + laptopPreference
-    totalScore = calculateScore();
+    var trait = parseInt($("#trait").val());
+    var whyCode = parseInt($("#whyCode").val());
+    var skill = parseInt($("#skill").val());
+    var interest = parseInt($("#interest").val());
+    var laptopPreference = parseInt($("#laptop").val());
+    console.log(trait)
+    console.log(whyCode)
+    console.log(skill)
+    console.log(interest)
+    console.log(laptopPreference)
+    var totalScore = trait + whyCode + skill + interest + laptopPreference
+    // totalScore = calculateScore();
     console.log(totalScore);
-    if(checkErrors()){
+    if(totalScore <5){
       alert("please enter all fields")
     }
     else if (totalScore >= 5 && totalScore < 9){
