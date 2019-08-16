263b68d2a09a8d2d40f719416300e55b25e11d5a
Nikhil Batta <nbatta08@gmail.com>
2019-08-16 10:06:10 -0700

    adds consolelog to check if my values are being correctly being parseINt

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,15 @@
 $(document).ready(function(){
   $("#formOne").submit(function(event){
-    //event.preventDefault();
-
+    event.preventDefault();
+    var trait = parseInt($("#trait").val());
+    var whyCode = parseInt($("#whyCode").val());
+    var skill = parseInt($("#skill").val());
+    var interest = parseInt($("#interest").val());
+    var laptopPreference = parseInt($("#laptop").val());
+    console.log(trait)
+    console.log(whyCode)
+    console.log(skill)
+    console.log(interest)
+    console.log(laptopPreference)
   })
 })
