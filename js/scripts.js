83b2859512e2262785944e7e394484960a6aee2e
Nikhil Batta <nbatta08@gmail.com>
2019-08-16 12:20:51 -0700

    adds functionality and trying to figure out better ways to write functions last log before lunch

--- js/scripts.js
+++ js/scripts.js
@@ -3,22 +3,53 @@ function refresh(){
   $(".field").val(0);
   $(".field").removeClass("one")
 }
+function checkErrors(){
+  var fields = $(".field").get();
+  var hasErrors = 0;
+  fields.forEach(function(questionIndex){
+    var fieldValue = parseInt($(questionIndex).val());
+
+    if (fieldValue === 0){
+      $(questionIndex).addClass("one");
+      hasErrors = true;
+    }
+    else {
+      $(questionIndex).removeClass("one")
+        console.log(questionIndex);
+    }
+  });
+  return hasErrors;
+}
+
+function calculateScore(){
+  var fields = $(".field").get();
+  var totalScore = 0 ;
+  fields.forEach(function(questionIndex){
+    var fieldValue = parseInt($(questionIndex).val());
+    totalScore = totalScore + fieldValue;
+    });
+  return totalScore;
+}
+
+
 $(document).ready(function(){
   $("#formOne").submit(function(event){
     event.preventDefault();
-    var trait = parseInt($("#trait").val());
-    var whyCode = parseInt($("#whyCode").val());
-    var skill = parseInt($("#skill").val());
-    var interest = parseInt($("#interest").val());
-    var laptopPreference = parseInt($("#laptop").val());
-    console.log(trait)
-    console.log(whyCode)
-    console.log(skill)
-    console.log(interest)
-    console.log(laptopPreference)
-    var totalScore = trait + whyCode + skill + interest + laptopPreference
-    if(totalScore < 5){
-      $(".field").addClass("one")
+    // var trait = parseInt($("#trait").val());
+    // var whyCode = parseInt($("#whyCode").val());
+    // var skill = parseInt($("#skill").val());
+    // var interest = parseInt($("#interest").val());
+    // var laptopPreference = parseInt($("#laptop").val());
+    // console.log(trait)
+    // console.log(whyCode)
+    // console.log(skill)
+    // console.log(interest)
+    // console.log(laptopPreference)
+    //var totalScore = trait + whyCode + skill + interest + laptopPreference
+    totalScore = calculateScore();
+    console.log(totalScore);
+    if(checkErrors()){
+      alert("please enter all fields")
     }
     else if (totalScore >= 5 && totalScore < 9){
     $("#selectOne").show();
