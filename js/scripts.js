5d174434c93b2a2ca0f96a7bfa21d22a7298e194
Nikhil Batta <nbatta08@gmail.com>
2019-08-16 11:13:01 -0700

    adds if else logic and trying to think of more logic to make code better

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,8 @@
+function refresh(){
+  $(".hide").hide();
+  $(".field").val(0);
+  $(".field").removeClass("one")
+}
 $(document).ready(function(){
   $("#formOne").submit(function(event){
     event.preventDefault();
@@ -12,5 +17,15 @@ $(document).ready(function(){
     console.log(interest)
     console.log(laptopPreference)
     var totalScore = trait + whyCode + skill + interest + laptopPreference
-  })
-})
+    if(totalScore < 5){
+      $(".field").addClass("one")
+    }
+    else if (totalScore >= 5 && totalScore < 9){
+    $("#selectOne").show();
+  } else if (totalScore >=9 && totalScore <14){
+    $("#selectTwo").show();
+  } else {
+    $("#selectThree").show();
+  }
+});
+});
